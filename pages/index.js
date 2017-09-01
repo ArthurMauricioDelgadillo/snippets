6b44d550edc6994c69a4929b3523ca2d267adccf
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-09-01 18:10:40 -0500

    styles(pages): add bootstrap css

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ValidationError, ValidationErrors } from '../validation-messages'
 
 let errors = new ValidationErrors(
@@ -7,25 +8,28 @@ let errors = new ValidationErrors(
 
 export default () => (
     <div>
-        <h3>Person registration form</h3>
-        <form>
-            <div className="form-group">
-                <label htmlFor="name">Name:</label>
-                <input id="name" name="name" className="form-control" type="text" />
-                {errors.name && <div className="alert alert-danger">{errors.name.message}</div>}
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input id="email" name="email" className="form-control" type="email" aria-describedby="emailHelp" />
-                <small id="emailHelp">We'll never share your email with anyone else.</small>
-                {errors.email && <div className="alert alert-danger">{errors.email.message}</div>}
-            </div>
-            <button type="submit">Register</button>
-        </form>
-        <style jsx>{`
-            .form-control {
-                display: block;
-            }
-        `}</style>
+        <Head>
+            <title>Demo :)</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+        </Head>
+        <div className="container">
+
+            <h1>Person registration form</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input id="name" name="name" className="form-control" type="text" />
+                    {errors.name && <div className="alert alert-danger">{errors.name.message}</div>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input id="email" name="email" className="form-control" type="email" aria-describedby="emailHelp" />
+                    <small id="emailHelp">We'll never share your email with anyone else.</small>
+                    {errors.email && <div className="alert alert-danger">{errors.email.message}</div>}
+                </div>
+                <button className="btn btn-primary" type="submit">Register</button>
+            </form>
+        </div>
     </div>
 )
